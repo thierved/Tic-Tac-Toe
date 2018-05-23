987628573355732974a5e66104956f5fe306ea77
Thierno Sadou Diallo <thierved01@gmail.com>
2018-05-23 13:49:44 +0000

    added game rule

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ function playDice() {
             currentPlayer = 1;
             putDice($(this).attr('id'), 'O');            
         };
+        checkWinner() 
     } )
 } 
 
@@ -31,4 +32,45 @@ function putDice(id, dice) {
     $activePlayer = $('#active-player')
                     .text(`It\'s Player ${currentPlayer}'s turn`);    
     $('#'+id).text(dice);
+}
+
+function checkWinner() {
+    isWinnerCell(p1);
+    isWinnerCell(p2);
+}
+
+function isWinnerCell(p=[]) {
+    let rows = [["1","2","3"], ["4","5","6"], ["7","8","9"]];
+    let cols = [["1","4","7"], ["2","5","8"], ["3","6","9"]];
+    let firsDiag = ['1','5','9'];
+    let secDiag = ['3','5','7'];
+
+    for(const row of rows) {
+        for (let col=0; col<3; col++) {
+            if (p.includes(row[col]) && p.includes(row[col+1]) && p.includes(row[col+2])) {
+                alertWinner(p);
+            }            
+        }
+    }
+
+    for(const col of cols) {
+        for (let row=0; row<3; row++) {
+            if (p.includes(col[row]) && p.includes(col[row+1]) && p.includes(col[row+2])) {
+                alertWinner(p);
+            }
+        }
+    }
+
+    for (let i = 0; i < p.length; i++) {
+        if (p.includes(firsDiag[i]) && p.includes(firsDiag[i+1]) && p.includes(firsDiag[i+2])) {
+            alertWinner(p);
+        }
+        if (p.includes(secDiag[i]) && p.includes(secDiag[i+1]) && p.includes(secDiag[i+2])) {
+            alertWinner(p);
+        }            
+    }
+}
+
+function alertWinner(p) {
+    alert(p === p1 ? 'p1' : 'p2' + ' is winner!');
 }
